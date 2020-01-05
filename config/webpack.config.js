ab026cd61dea6600179a35d693bfe342a66a07cb
Christopher <easonmu@hotmail.com>
2020-01-06 00:11:33 +0800

    m

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,43 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// import HtmlWebpackPlugin from 'html-webpack-plugin'
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+
+const path = require('path')
+const fs = require('fs')
+
+const appDirectory = fs.realpathSync(process.cwd())
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(appDirectory, 'build'),
     filename: '[name].[contenthash:8].bundle.js'
   },
   mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'/* , { useBuiltIns: 'entry' } */],
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin()
   ]
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-let myvar = 'ggggggggggg'
-let myvar2 = 'ffffffff'
+import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
-let bar = function() {
-  console.log(6666)
-  let returnValue = 'barfffffff'
-  return returnValue
-}
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('app')
+// );
 
-let foo = function() {
-  let returnValue = bar()
-  return returnValue
-}
-
-myvar = foo()
-myvar2 = myvar
-
-// function component() {
-//   let element = document.createElement('div')
-
-//   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-//   element.innerHTML = '6666'
-
-//   return element
-// }
-
-// document.body.appendChild(component())
+console.log(App)
